bf40d27bd6f836e9ef5dd9a42d209c636a588338
davidoc23 <g00400530@atu.ie>
2023-12-04 18:54:52 +0000

    L9 - detected the ORB keypoints and displayed them

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -113,3 +113,27 @@ imgORB = cv2.drawKeypoints(img, keypoints, None, color=(0, 255, 0), flags=0)
 cv2.imshow('ORB Keypoints', imgORB)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Exercise 17: Feature matching with ORB
+img1 = cv2.imread('ATU1.jpg', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('ATU2.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Use ORB to detect keypoints and descriptors
+orb = cv2.ORB_create()
+keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+
+# Use BruteForceMatcher
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches = bf.match(descriptors1, descriptors2)
+
+# Sort them in ascending order of distance
+matches = sorted(matches, key=lambda x: x.distance)
+
+# Draw the matches
+img_matches = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# Display the matches
+cv2.imshow('ORB Matches', img_matches)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
