1b8db20c41a2e51e0ca1aa27431d6457a71371cc
davidoc23 <g00400530@atu.ie>
2023-12-04 19:02:10 +0000

    L9 - Contour detection for image 2

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -175,3 +175,41 @@ thresh_callback(thresh)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#Do the same for the second image
+def thresh_callback(val):
+    threshold = val
+    # Detect edges using Canny
+    canny_output = cv2.Canny(src_gray, threshold, threshold * 2)
+    # Find contours
+    contours, hierarchy = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Draw contours
+    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+    for i in range(len(contours)):
+        color = (np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256))
+        cv2.drawContours(drawing, contours, i, color, 2, cv2.LINE_8, hierarchy, 0)
+    # Show in a window
+    cv2.imshow('Contours', drawing)
+
+# Load source image
+src = cv2.imread('ATU2.jpg')  # Replace 'your_image.jpg' with the actual filename and path
+if src is None:
+    print('Could not open or find the image.')
+    exit(0)
+
+# Convert image to gray and blur it
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+src_gray = cv2.blur(src_gray, (3, 3))
+
+# Create Window
+source_window = 'Source'
+cv2.namedWindow(source_window)
+cv2.imshow(source_window, src)
+
+max_thresh = 255
+thresh = 100  # initial threshold
+cv2.createTrackbar('Canny Thresh:', source_window, thresh, max_thresh, thresh_callback)
+thresh_callback(thresh)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
