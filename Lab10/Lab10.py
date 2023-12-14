9153b65edd498a5b300bedff2ea033bb2dbf5cf3
davidoc23 <g00400530@atu.ie>
2023-12-14 22:28:03 +0000

    L10 - Added the video input paths and output paths

--- Lab10/Lab10.py
+++ Lab10/Lab10.py
@@ -3,3 +3,7 @@ from ultralytics import YOLO
 
 # Load the YOLOv8 model
 model = YOLO('yolov8n.pt')
+
+# List of video paths
+video_paths = ["traffic3.mp4", "traffic.mp4"]
+output_paths = ["output_traffic3.mp4", "output_traffic.mp4"]
