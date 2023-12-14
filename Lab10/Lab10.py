2cec65fb822c416ae932ec29717364de3eb8d8ee
davidoc23 <g00400530@atu.ie>
2023-12-14 22:30:26 +0000

    L10 - persistant tracking and output result to new files

--- Lab10/Lab10.py
+++ Lab10/Lab10.py
@@ -7,3 +7,41 @@ model = YOLO('yolov8n.pt')
 # List of video paths
 video_paths = ["traffic3.mp4", "traffic.mp4"]
 output_paths = ["output_traffic3.mp4", "output_traffic.mp4"]
+
+for i, video_path in enumerate(video_paths):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Adjust the codec based on your preference
+    output_path = output_paths[i]
+    output_video = cv2.VideoWriter(output_path, fourcc, 25.0, (int(cap.get(3)), int(cap.get(4))))
+
+    # Loop through the video frames
+    while cap.isOpened():
+        # Read a frame from the video
+        success, frame = cap.read()
+
+        if success:
+            # Run YOLOv8 tracking on the frame, persisting tracks between frames
+            results = model.track(frame, persist=True)
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+            # Write the frame to the output video
+            output_video.write(annotated_frame)
+
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            # Break the loop if the end of the video is reached
+            break
+
+    # Release the video capture object and the VideoWriter
+    cap.release()
+    output_video.release()
